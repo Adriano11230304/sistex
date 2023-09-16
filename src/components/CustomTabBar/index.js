acea4b43ab5e83a0dd87f837ef0b96242b373c47
Adriano11230304 <adriano.al.pereira1@gmail.com>
2023-09-16 13:22:43 -0300

    ajustes

---
+++ src/components/CustomTabBar/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+const CustomTabBar = () => {
+    return (
+        <View>
+            <Text>cdscsdcsdcsd</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    
+});
+
+export default CustomTabBar;
--- src/routes/TabRoutes.js
+++ src/routes/TabRoutes.js
@@ -4,8 +4,11 @@ import Notifications from '../pages/Notifications';
 import Login from '../pages/Login';
 import { useAuth } from '../store/auth'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import CustomTabBar from '../components/CustomTabBar';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { AntDesign } from '@expo/vector-icons';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 export default function TabRoutes() {
@@ -14,8 +17,22 @@ export default function TabRoutes() {
     return (
         <>
             {state.signed ? (
-                <Tab.Navigator screenOptions={{ headerShown: false }}>
-                    <Tab.Screen name="Home" component={Home} />
+                <Tab.Navigator 
+                screenOptions={{ 
+                    tabBarHideOnKeyboard: true,
+                    tabBarShowLabel: true,
+                    tabBarStyle: {
+                        borderTopWidth: 0,
+                        //backgroundColor: '#fff'
+                    }
+                }}
+
+                // tabBar={(props) => <CustomTabBar {...props}/>}
+                >
+                    <Tab.Screen options={{
+                        tabBarShowLabel: false,
+                        tabBarIcon: ({ color }) => (<AntDesign name="home" size={24} color="black" />)
+                        }} name="Home" component={Home} />
                     <Tab.Screen name="Notifications" component={Notifications} />
                     {/*<Tab.Screen name="Contas a pagar" component={Home} />
                     <Tab.Screen name="Contas a receber" component={Home} />
@@ -24,7 +41,7 @@ export default function TabRoutes() {
                 </Tab.Navigator>
             ): (
                 <Stack.Navigator screenOptions={{ headerShown: false }}>
-                    <Tab.Screen name="Login" component={Login} />
+                    <Tab.Screen  name="Login" component={Login} />
                 </Stack.Navigator>
             )}
         </>
