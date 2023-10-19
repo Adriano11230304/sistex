5de7ad9cd05486f7dd263ec77e6e893cc3598405
adriano11230304 <adriano.al.pereira1@gmail.com>
2023-10-19 16:17:21 -0300

    ajustes

--- src/pages/Fornecedores/index.js
+++ src/pages/Fornecedores/index.js
@@ -70,55 +70,12 @@ export default function Fornecedores({ navigation, route }) {
         navigation.navigate('AddFornecedor');
     }
 
+    const setText = () => {
+        console.log("csdcsdcsd");
+    }
+
 
     return (
-        <SafeAreaView style={styles.container}>
-            <Header />
-            <View style={styles.title}>
-                <Text style={styles.text}>Fornecedores</Text>
-            </View>
-            <View style={styles.searchArea}>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Pesquise por um fornecedor"
-                    placeholderTextColor="#888"
-                    value={searchText}
-                    onChangeText={(t) => setSearchText(t)}
-                />
-                <FontAwesome name="search" size={24} color="black" />
-            </View>
-            {loading ? (
-                <>
-                    <LoaderSimple/>
-                </>
-            ) : (
-                <>
-                    <FlatList
-                        showsVerticalScrollIndicator={false}
-                        data={state.fornecedores}
-                        renderItem={({ item }) => 
-                            <View style={styles.itemList}>
-                                <View style={styles.list}>
-                                    <Text style={styles.textList}>{item.name}</Text>
-                                    <Text style={styles.textList}>{item.cnpj}</Text>
-                                </View>
-                                <View style={styles.buttons}>
-                                    <TouchableOpacity onPress={() => editFornecedor(item.id)}><Text style={styles.buttonText}><AntDesign name="edit" size={24} color="black" /></Text></TouchableOpacity>
-                                    <TouchableOpacity onPress={() => deleteFornecedor(item.id)}><Text style={styles.buttonText}><MaterialCommunityIcons name="delete" size={24} color="black" /></Text></TouchableOpacity>
-                                </View>
-                            </View>
-                        }
-                        keyExtractor={(item) => item.id}
-                    />
-
-                    <View style={styles.buttonAdd}>
-                        <TouchableOpacity onPress={addFornecedores}>
-                            <AntDesign name="pluscircleo" size={50} color="black" />
-                        </TouchableOpacity>
-                    </View>
-                </>
-            )}
-            
-        </SafeAreaView>
+        <List list={state.fornecedores} edit={editFornecedor} del={deleteFornecedor} add={addFornecedores} loading={loading} searchText={searchText} />
     );
 }
